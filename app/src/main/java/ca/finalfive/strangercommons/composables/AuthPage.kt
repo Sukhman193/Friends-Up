45aa82a140c887d0f7fb5dafee8b9eec6d687090
Lordsahandii <Sahand2k24@gmail.com>
2022-11-02 22:35:24 -0700

    Update AuthPage.kt

--- app/src/main/java/ca/finalfive/strangercommons/composables/AuthPage.kt
+++ app/src/main/java/ca/finalfive/strangercommons/composables/AuthPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,10 +22,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import ca.finalfive.strangercommons.R
+import ca.finalfive.strangercommons.navigation.Navigation
 import ca.finalfive.strangercommons.viewmodels.AuthViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -33,11 +37,13 @@ import kotlinx.coroutines.tasks.await
 
 
 @Composable
-fun AuthPage(authViewModel: AuthViewModel){
+fun AuthPage(authViewModel: AuthViewModel, navController: NavController){
 
     val context = LocalContext.current
     val token = stringResource(R.string.default_web_client_id)
     val scope = rememberCoroutineScope()
+
+    var user: FirebaseUser? = null
 
     val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
         val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
@@ -64,7 +70,7 @@ fun AuthPage(authViewModel: AuthViewModel){
         ),
 
     ) {
-        Text(text = "Stranger Commons", fontFamily = Font(R.font.hello_valentina))
+        Text(text = "Stranger Commons", style = MaterialTheme.typography.h3)
         
         Text(text = "Are You Ready For The Toughest Challenges Of All?")
 
@@ -74,18 +80,12 @@ fun AuthPage(authViewModel: AuthViewModel){
             modifier = Modifier
                 .clickable(true, onClick = {
                     authViewModel.signIn(token, context, launcher)
+                    user = authViewModel.user
+                    Log.d("USER->>>", user?.email.toString())
+//                    navController.navigate("")
                 })
                 .size(122.dp)
         )
     }
-
-    Button(onClick = {
-        authViewModel.signIn(token,context,launcher)
-        Log.d("AUTH -------------> ", authViewModel.user?.displayName.toString())
-
-    }) {
-
-    }
-
 
 }
