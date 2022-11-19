727341ace796db37f237e2ef511e66e74e300675
Sukhman193 <83609340+Sukhman193@users.noreply.github.com>
2022-11-18 16:28:27 -0800

    solved merging conflict

--- app/src/main/java/ca/finalfive/friendsup/screens/ProfileScreen.kt
+++ app/src/main/java/ca/finalfive/friendsup/screens/ProfileScreen.kt
@@ -7,27 +7,6 @@ import ca.finalfive.friendsup.composables.ProfilePage
  * Profile Screen - navigates to the Profile Page
  */
 @Composable
-<<<<<<< HEAD
-fun ProfileScreen(navController: NavController) {
-    val (openReportPopup, setReportPopup) = remember {
-        mutableStateOf(false)
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column() {
-            Text(
-                text = "Profile page",
-                modifier = Modifier.clickable {
-                    setReportPopup(true)
-                }
-            )
-        }
-        if(openReportPopup) {
-//            ReportPopup(setReportPopup = setReportPopup)
-        }
-    }
-=======
 fun ProfileScreen() {
     ProfilePage()
->>>>>>> development
 }
