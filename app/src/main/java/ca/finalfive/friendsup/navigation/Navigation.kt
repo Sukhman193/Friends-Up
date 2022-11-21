4bd2e3ade173e2ca139fca8f659e405f2a075ac4
Lordsahandii <Sahand2k24@gmail.com>
2022-11-20 20:43:34 -0800

    merged the code with development

--- app/src/main/java/ca/finalfive/friendsup/navigation/Navigation.kt
+++ app/src/main/java/ca/finalfive/friendsup/navigation/Navigation.kt
@@ -2,7 +2,6 @@ package ca.finalfive.friendsup.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
