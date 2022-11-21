bbe0f9a73c1b9d0fc4d308092e2fb2e6dcb1ff53
Lorenz Castillo <lorenzcastillo@Lorenzs-MacBook-Air.local>
2022-11-21 00:21:13 -0800

    Removed the preview function out of QuestionOption.kt

--- app/src/main/java/ca/finalfive/friendsup/composables/QuestionOption.kt
+++ app/src/main/java/ca/finalfive/friendsup/composables/QuestionOption.kt
@@ -1,13 +1,11 @@
 package ca.finalfive.friendsup.composables
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,12 +14,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.times
 import ca.finalfive.friendsup.R
 import ca.finalfive.friendsup.animations.ButtonAnimation
 import ca.finalfive.friendsup.composables.utils.ProfileIcon
@@ -147,58 +141,6 @@ fun QuestionOption(
                 }
             }
         }
-    }
-    // Spacer between the options
-    Spacer(
-        modifier = Modifier
-            .padding(bottom = 12.dp)
-    )
-}
-
-@Preview
-@Composable
-fun Preview(onClick: () -> Unit = {}) {
-    var isClicked by remember {
-        mutableStateOf(false)
-    }
-
-    // The entire button size (with the darker color as the background)
-    Box(
-        // Darker part of the button
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 30.dp)
-            .height(70.dp)
-            .clip(RoundedCornerShape(25.dp))
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onDoubleTap = {
-                        onClick()
-                        isClicked = true
-                    }
-                )
-            }
-            .background(colorResource(R.color.dark_purple))
-    ) {
-        // Lighter color on the left of the button
-        Box(
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(50.dp)
-                .background(colorResource(R.color.light_purple))
-        )
-
-        // Text inside the button
-        Text(
-            text = "Option",
-            color = Color.White,
-            style = MaterialTheme.typography.h3,
-            fontSize = 20.sp,
-            modifier = Modifier
-                .align(Alignment.CenterStart)
-                .padding(start = 65.dp)
-                .padding(bottom = 10.dp)
-        )
     }
     // Spacer between the options
     Spacer(
