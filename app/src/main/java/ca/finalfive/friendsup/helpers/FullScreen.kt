1136281572fa4685a01da569d3c18ebbf4d7d456
Sukhmandeep Singh <sukhmandeepsingh@Sukhmandeeps-MacBook-Air.local>
2022-11-25 12:32:20 -0800

    Fixed bug of in game progress for chat

--- app/src/main/java/ca/finalfive/friendsup/helpers/FullScreen.kt
+++ app/src/main/java/ca/finalfive/friendsup/helpers/FullScreen.kt
@@ -4,7 +4,6 @@ import android.os.Build
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import android.view.WindowManager
 
 /**
  * Make the application full screen depending on which
@@ -12,18 +11,20 @@ import android.view.WindowManager
  * @param window window created during the application launch
  */
 fun makeApplicationFullScreen(window: Window) {
-    // If the current sdk is lower than 30, than use the
-    // older flag to set the full screen mode
+    // If the current sdk is lower than 30, than we return
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-        // Turn on full screen mode on older versions of android
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-    } else {
-        // Hide the status bar for modern versions of android
-        window.insetsController?.apply {
-            // Hide status bar
-            hide(WindowInsets.Type.statusBars())
-            // Add status bar behaviour for drop down
-            systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
+        // Setting the full screen mode for android versions
+        // which are below API 30 will conflict with the
+        // WindowCompat.setDecorFitsSystemWindows() as a
+        // result full screen mode will not be available
+        // for them
+        return
+    }
+    // Hide the status bar for modern versions of android
+    window.insetsController?.apply {
+        // Hide status bar
+        hide(WindowInsets.Type.statusBars())
+        // Add status bar behaviour for drop down
+        systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
